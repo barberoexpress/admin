3be9617dcc284317fc2ce57efbac65f332264868
stiago <stiago01r@gmail.com>
2017-02-07 14:24:57 -0500

    kaka
    esto lo voy a quitar

--- public/index.js
+++ public/index.js
@@ -1,6 +1,12 @@
 const app = require('./app');
 const config = require('./config')
 
+var firebase =require('firebase');
+
+firebase.initializeApp({
+  serviceAccount:"../adminBarberoExpress-bd7661c38f23.json",
+  databaseURL:"https://adminbarberoexpress-33fb4.firebaseio.com"
+});
 
 app.listen(config.port , function (){
     console.log(`Aplicación corriendo en htpp://localhost:${config.port}`)
